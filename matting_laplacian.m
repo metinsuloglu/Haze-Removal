e2b37a40d92051cfd3e1451fc7d5ce625107b40b
Metin Suloglu <metinsuloglu97@gmail.com>
2018-10-29 13:21:21 +0300

    Speed improvements

--- matting_laplacian.m
+++ matting_laplacian.m
@@ -28,41 +28,41 @@ U = eye(3);
 
 f = waitbar(0, 'Computing the matting Laplacian matrix...');
 
-for k = 1:img_size
+for rk = 1:M
+    for ck = 1:N
+        
+        % Lower and upper corners of window
+        [lrw_k, lcw_k, urw_k, ucw_k] = deal(max(1, rk - win_radius),...
+            max(1, ck - win_radius), min(M, rk + win_radius),...
+            min(N, ck + win_radius));
+        
+        % Window k of image I
+        w_k = I(lrw_k:urw_k, lcw_k:ucw_k, :);
+        
+        win_indices = img_indices(lrw_k:urw_k, lcw_k:ucw_k);
+        [Mw, Nw, ~] = size(w_k);
+        n_pixels = Mw * Nw;
+        extent = n_pixels ^ 2;
+        w_k = reshape(w_k, n_pixels, 3);
+        
+        mu_k = mean(w_k, 1);
+        Sigma_k = cov(w_k, 1);
+        
+        w_k = w_k - mu_k;
+        
+        L_ij = (1 + (w_k / (Sigma_k + (epsilon / n_pixels) * U)) * w_k') / n_pixels;
+        
+        win_indices = repmat(win_indices(:), 1, n_pixels);
+        row_indices(s_len + 1:s_len + extent) = win_indices(:);
+        win_indices = win_indices';
+        column_indices(s_len + 1:s_len + extent) = win_indices(:);
+        values(s_len + 1:s_len + extent) = L_ij(:);
+        
+        s_len = s_len + n_pixels ^ 2;
+        
+    end
     
-    [rk, ck] = ind2sub([M, N], k);
-    
-    % Lower and upper corners of window
-    [lrw_k, lcw_k, urw_k, ucw_k] = deal(max(1, rk - win_radius),...
-        max(1, ck - win_radius), min(M, rk + win_radius),...
-        min(N, ck + win_radius));
-    
-    % Window k of image I
-    w_k = I(lrw_k:urw_k, lcw_k:ucw_k, :);
-    
-    win_indices = img_indices(lrw_k:urw_k, lcw_k:ucw_k);
-    [Mw, Nw, ~] = size(w_k);
-    n_pixels = Mw * Nw;
-    extent = n_pixels ^ 2;
-    w_k = reshape(w_k, n_pixels, 3);
-    
-    mu_k = mean(w_k, 1);
-    Sigma_k = cov(w_k, 1);
-    
-    w_k = w_k - mu_k;
-    
-    L_ij = (1 + w_k * ((Sigma_k + epsilon / n_pixels * U) \ w_k')) / n_pixels;
-    
-    win_indices = repmat(win_indices(:), 1, n_pixels);
-    row_indices(s_len + 1:s_len + extent) = win_indices(:);
-    win_indices = win_indices';
-    column_indices(s_len + 1:s_len + extent) = win_indices(:);
-    values(s_len + 1:s_len + extent) = L_ij(:);
-    
-    s_len = s_len + n_pixels ^ 2;
-    
-    waitbar(k * 0.95 / img_size);
-    
+    waitbar(rk * 0.98 / M);
 end
 
 % Create sparse matrix L
