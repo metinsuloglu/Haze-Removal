3f7aae9f84e0780bf9fd72801ec5c60a2a6d6779
Metin Suloglu <metinsuloglu97@gmail.com>
2018-12-16 20:48:17 +0300

    Revert changes

--- atmospheric_light.m
+++ atmospheric_light.m
@@ -14,23 +14,19 @@ if nargin == 2
     percentage = 0.001;
 end
 
-[M, N, C] = size(I);
+[M, N, ~] = size(I);
 n_pixels = M * N;
 dc = dc(:);
 I = reshape(I, [n_pixels, 1, 3]);
 
 k = ceil(n_pixels * percentage);
 
-[~, indeces] = sort(dc(:), 'descend');
-img_sub = zeros(1, k, C);
-for rgb = 1:C
-    I_rgb = I(:, :, rgb);
-    img_sub(:, :, rgb) = I_rgb(indeces(1:k));
-end
+[~, indeces] = sort(dc, 'descend');
+img_sub = I(indeces(1:k), :, :);
 
 [~, max_index] = max(vecnorm(img_sub, 2, 3));
 
-A = img_sub(1, max_index, :);
+A = img_sub(max_index, :);
 
 end
 
